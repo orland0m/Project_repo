73c2977b23e1ff32d0d915ed3ea7a82fa8d60778
orland0m <orland0m@ucla.edu>
2013-01-30 18:30:34 -0800

    Trying to fix type error

--- cache.cc
+++ cache.cc
@@ -38,7 +38,7 @@ char * GetFromCache(string file){
 	if(dataLength>1){
 		try{
 			HttpResponse * response = new HttpResponse;
-			response -> ParseResponse(data.c_str(),dataLength);
+			response -> ParseResponse((const char *)(data.c_str()),dataLength);
 			if(isExpired(response -> FindHeader("Expires"))){
 				//remove(file);
 				delete response;

1025721456966af5b843eefebfd3223fe07a214b
orland0m <orland0m@ucla.edu>
2013-02-06 22:07:44 -0800

    Testing

--- http-proxy.cc
+++ http-proxy.cc
@@ -9,11 +9,11 @@
 using namespace std;
 
 int main (int argc, char *argv[]){
-	string rq = "GET http://www.google.com/ HTTP/1.1\r\n\r\n";
+	string rq = "GET http://www.google.com/index.html HTTP/1.0\r\n\r\n";
 	HttpRequest * request = new HttpRequest;
 	request -> ParseRequest(rq.c_str(), rq.length());
-	string response;
-	response = GetFromCache(request, 0); // get non expired file from cache
+	cout << "File: " << request -> GetHost() << request -> GetPath();
+	string response = GetFromCache(request, 0); // get non expired file from cache
 	if(response.length()>0){
 		cout << "Bingo! your file is in: cache/" << request->GetHost() << request->GetPath() << endl;
 		return 0;
@@ -21,7 +21,7 @@ int main (int argc, char *argv[]){
 		cout << "Oops... This is embarrassing. We don't have that file yet" << endl;
 	}
 	cout << "Making remote request..." << endl;
-	response = GetFromRemoteServer(request,0); //requesting to remote server, the second argument should be a socket file descriptor
+	response = GetFromRemoteServer(request, 0); //requesting to remote server, the second argument should be a socket file descriptor
 	cout << "Response arrived: " << response <<endl;
 	// response should contain a file ready to be sent to the client, even if there was an error
 	return 0;

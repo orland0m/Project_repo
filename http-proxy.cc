a97e0c73719724e5ee2847111975407d112ccd39
orland0m <orland0m@ucla.edu>
2013-02-09 14:18:13 -0800

    Testing remote request

--- http-proxy.cc
+++ http-proxy.cc
@@ -37,7 +37,7 @@ int main (int argc, char *argv[]){
 	}
 	cout << "Making remote request..." << endl;
 	
-	string destPort = request->GetPort()+""; // port
+	string destPort = 80+""; // port
 	string destHost = string(request->GetHost()); // host URL
 	cout << destPort << ": Port" << endl;
 	cout << destHost << ": Host" << endl;

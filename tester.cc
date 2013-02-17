15a5e108c6b772dce67ec63e1d2bf515c98fed9f
orland0m <orland0m@ucla.edu>
2013-02-17 00:28:40 -0800

    Fixing

--- tester.cc
+++ tester.cc
@@ -6,7 +6,7 @@ using namespace std;
 using namespace boost::filesystem;
 int main(){
 	string path_file = "boost/tester/abc.txt";
-	path file = path(path);
+	path file = path(path_file);
 	cout << file.root_directory() << endl;
 	cout << file.root_path() << endl;
 	//create_directories();

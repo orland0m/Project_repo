835e38382aab738b4bfb772781e363ba4842a8b1
orland0m <orland0m@ucla.edu>
2013-02-17 09:45:29 -0800

    Testing date

--- tester.cc
+++ tester.cc
@@ -12,15 +12,21 @@ int isExpired(string date){
 	int expired = 1;
 	try{
 		ptime pt;
-		time_input_facet * format = new time_input_facet("%a, %d %b %Y %H:%M:%S %Z");
-		const locale loc = locale(locale::classic(),format);
+		const locale loc = locale(locale::classic(),new time_input_facet("%a, %d %b %Y %H:%M:%S %Z"));
 		
 		istringstream is(date);
         is.imbue(loc);
         is >> pt;
-        cout << pt << endl;
-		
-		delete format;
+        
+        ptime curr_time(second_clock::local_time());
+        cout << "Current time: " << curr_time << endl;
+        cout << "File time: " << pt << endl;
+		if(cur_time>pt){
+			cout << "Current time is greater" << endl;
+		}else{
+			cout << "File time is greater" << endl;
+		}
+		//delete format;
 	}catch(...){
 		expired = 1;
 	}
@@ -30,4 +36,5 @@ int isExpired(string date){
 
 int main(){
 	isExpired("Thu, 01 Dec 1994 16:00:00 GMT");
+	isExpired("Sun, 17 Feb 2013 16:00:00 GMT");
 }

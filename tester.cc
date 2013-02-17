dd9fab00c96a605e28d88b94a25f295904586d2f
orland0m <orland0m@ucla.edu>
2013-02-17 00:31:07 -0800

    Test

--- tester.cc
+++ tester.cc
@@ -8,6 +8,6 @@ int main(){
 	string path_file = "boost/tester/abc.txt";
 	path file = path(path_file);
 	cout << file.filename() << endl;
-	cout << file.relative_path() << endl;
+	cout << file.parent_path() << endl;
 	//create_directories();
 }

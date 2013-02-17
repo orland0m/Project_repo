1f074ad193affed30c64e81983432d7475592c0a
orland0m <orland0m@ucla.edu>
2013-02-16 21:30:23 -0800

    Testing boost

--- http-proxy.cc
+++ http-proxy.cc
@@ -2,6 +2,9 @@
 #include <fstream>
 #include <string>
 #include <sys/socket.h>
+#include <boost/date_time/posix_time/posix_time.hpp>
+
+
 
 //project wireframe
 #include "http-request.h"
@@ -45,7 +48,34 @@
 
 using namespace std; 
 
-using namespace std;
+
+void test_boost(){
+	
+    using namespace boost::gregorian;
+    using namespace boost::posix_time;
+    using namespace std;
+
+    stringstream ss;
+
+    /****** format strings ******/
+    time_facet* output_facet = new time_facet();
+    time_input_facet* input_facet = new time_input_facet();
+    ss.imbue(locale(ss.getloc(), output_facet));
+    ss.imbue(locale(ss.getloc(), input_facet));
+    output_facet->format(time_facet::iso_time_format_extended_specifier);
+    input_facet->format(time_input_facet::iso_time_format_extended_specifier);
+    ptime t(second_clock::local_time());
+    ss << t;
+    cout << ss.str() << endl; // 2012-08-03 23:46:38
+    ss.str("2000-01-31 12:34:56");
+    ss >> t;
+    assert(t.date().year() == 2000);
+    assert(t.date().month() == 1);
+    assert(t.date().day() == 31);
+    assert(t.time_of_day().hours() == 12);
+    assert(t.time_of_day().minutes() == 34);
+    assert(t.time_of_day().seconds() == 56);
+}
 
 string ProcessRequest(string rq, int& closeCon){
 	HttpRequest * request = new HttpRequest;
